7e1ba3d7408809df523a0df3496fc15059bf0666
Nithin <nithinkatkam504106@gmail.com>
2024-03-24 20:05:29 +0530

    done

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,10 @@ export default function Home() {
   return (
 <main className="flex min-h-screen flex-col items-center justify-between p-24">
   <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-    <p className="text-center text-gray-800 leading-relaxed">
+    <p className="text-center text-blue-800 leading-relaxed">
       Livepeer video creators can easily create custom contracts that are securely stored in the Tableland database. Using our Privy setup, creators can upload their livestream playback URLs or any video URLs in a secure and abstracted way.
     </p>
-    <p className="mt-4 text-center text-gray-800 leading-relaxed">
+    <p className="mt-4 text-center text-blue-800 leading-relaxed">
       Once uploaded, they will receive a unique frame link where they can paste their URL into WarpCast. This allows anyone to view the video by paying with ETH or native tokens, all within the frame itself. Our platform seamlessly integrates with Privy, making it easy for creators to manage and share their content.
     </p>
     <div className="mt-8 flex justify-center">
