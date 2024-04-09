01de3d1db3e26d00d3f107c324827430fb8c8545
Nithin <nithinkatkam504106@gmail.com>
2024-04-09 21:23:01 +0530

    fix

--- lib/livepeer.ts
+++ lib/livepeer.ts
@@ -5,7 +5,7 @@ import { Livepeer } from 'livepeer';
 export const createStream= async (streamData: { name: string }) => {
 	const url = 'https://livepeer.studio/api/stream';
 	const headers = {
-		Authorization:  `Bearer 41c68e93-8b26-4e49-834c-458b015ba152`,
+		Authorization:  `Bearer 59d06e44-f100-4e83-bf82-e75de9047766`,
 		'Content-Type': 'application/json',
 	};
 	const response = await axios.post(url, streamData, { headers });
